be91bf0f0e3e47a0580a86d534688014048dd4c2
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2022-07-15 21:52:46 -0300

    cards dinamicos

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ CRIAR CARD DA TEMPORADA SELECIONADA
 ADICIONAR UM EVENTO PARA ADICIONAR A TEMPORADA A FILA
 LISTAR AS TEMPORADAS SELECIONADAS
 REMOVERA TEMPORADA SELECIONADA
+FAZER FILTROS
 */
 
 //CRIAR CARD DA TEMPORADA
 const container = document.querySelector('.main_vitrine')
+
+const playlistContainer = document.querySelector("#playlist_container")
+
+let temporadasSelecionadas = []
 
 function cardPrincipal(temporada) {
     const article = document.createElement('article')
@@ -52,3 +57,74 @@ function listarTemporadas() {
 listarTemporadas()
 
 //CRIAR CARD DA TEMPORADA SELECIONADA
+function cardTemporadaSelecionada(temporada) {
+    const li = document.createElement("li")
+
+    const imgTemporada = document.createElement("img")
+    imgTemporada.src = temporada.imagemIcone
+    imgTemporada.alt = temporada.nome
+
+    const playlistCenter = document.createElement("div")
+    playlistCenter.classList.add ("playlist_li_center")
+
+    playlistCenter.insertAdjacentHTML("afterbegin", `
+        <div>
+            <h4>${temporada.nome}</h4>
+            <span>${temporada.temporada}</span>
+        </div>
+
+        <div class="playlist_li_playbutton">
+            <img src="assets/play-icon.png" alt="ícone de play">
+            <span>Assistir agora</span>
+        </div> `) //insere elementos html dentro de um elemento principar. Devemos definir uma posição
+
+    const playlistEnd = document.createElement("div")
+    playlistEnd.classList.add("playlist_li_end")
+
+    const button = document.createElement("button")
+    button.id = temporada.id
+
+    playlistEnd.appendChild(button)
+
+    li.append(imgTemporada, playlistCenter, playlistEnd)
+
+    return li
+}
+
+//ADICIONAR UM EVENTO PARA ADICIONAR A TEMPORADA A FILA
+container.addEventListener("click", selecionarTemporada)
+function selecionarTemporada(event) {
+
+
+    const elementoHTML = event.target
+
+    if (elementoHTML.tagName == "BUTTON") {
+        const idTemporada = elementoHTML.id
+
+        const temporadaEncontrada = temporadas.find((temporada) => temporada.id == idTemporada)
+
+        temporadasSelecionadas.push(temporadaEncontrada) //push = adiciona
+
+        listarTemporadasSelecionadas()
+    }
+}
+
+function listarTemporadasSelecionadas() {
+    playlistContainer.innerHTML = ""
+    for(let i = 0; i < temporadasSelecionadas.length; i++) {
+        const card = cardTemporadaSelecionada(temporadasSelecionadas[i])
+        playlistContainer.appendChild(card)
+    }
+}
+
+playlistContainer.addEventListener("click", function() {
+    const elementoHTML = event.target
+    if (elementoHTML.tagName == "BUTTON") {
+        elementoHTML.closest("li").remove()
+        
+        //capturar o id do button em uma variavel
+        //usar o find para encontrar a temporada que tem o mesmo id do button (https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/find) 
+        //usar o indexOf para encontrar o indice daquela temporada no array temporadasSelecionadas (https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf)
+        //remover a temporada com o splice (https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/splice)
+    }
+})
